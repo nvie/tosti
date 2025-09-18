24ab68ba8d675750a7f4fa617f3785c04325c071
Vincent Driessen <me@nvie.com>
2025-09-18 12:25:14 +0200

    Project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,79 @@
+import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tsParser from "@typescript-eslint/parser";
+import eslint from "@eslint/js";
+
+export default tseslint.config(
+  { ignores: ["dist/*", "coverage/*", "node_modules/*"] },
+
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+
+  {
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      // Each project's individual/local tsconfig.json defines the behavior
+      // of the parser
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  },
+
+  // -----------------------------
+  // Enable these checks
+  // -----------------------------
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+
+    rules: {
+      eqeqeq: ["error", "always", { null: "ignore" }],
+      "object-shorthand": "error",
+
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unnecessary-condition": ["error", { allowConstantLoopConditions: true }],
+      "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true }],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { args: "all", argsIgnorePattern: "^_.*", varsIgnorePattern: "^_.*" },
+      ],
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+
+      // --------------------------------------------------------------
+      // "The Code is the To-Do List"
+      // https://www.executeprogram.com/blog/the-code-is-the-to-do-list
+      // --------------------------------------------------------------
+      "no-warning-comments": ["error", { terms: ["xxx"], location: "anywhere" }],
+    },
+  },
+
+  // -------------------------------
+  // Disable these checks
+  // -------------------------------
+  {
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/unified-signatures": "off",
+      "@typescript-eslint/no-deprecated": "off",
+    },
+  },
+
+  // Overrides for tests specifically
+  {
+    files: ["test/**"],
+
+    // Relax ESLint a bit in tests
+    rules: {
+      "@typescript-eslint/no-confusing-void-expression": "off",
+    },
+  },
+);
