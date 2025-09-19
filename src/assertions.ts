3bd4a40d5def15d6b574e67462064f66a291b5db
Vincent Driessen <me@nvie.com>
2025-09-19 13:59:40 +0200

    Put expectors in separate module

--- src/assertions.ts
+++ src/assertions.ts
@@ -1,93 +1,7 @@
-import type { Decoder } from "decoders";
-import {
-  constant,
-  date,
-  exact,
-  formatInline,
-  isDecoder,
-  isPromiseLike,
-  poja,
-  pojo,
-  regex,
-  tuple,
-  unknown,
-} from "decoders";
+import { formatInline, isPromiseLike } from "decoders";
 
-import { isPojo } from "./decoders";
+import { literally, makeExpector } from "./expectors";
 import { fail } from "./TostiError";
-import { mapValues } from "./utils";
-
-type Expecter = Decoder<unknown>;
-
-function makeArrayExpecter(expected: unknown[]): Expecter {
-  return poja
-    .reject((arr) =>
-      arr.length > expected.length
-        ? `Too many elements, expected ${expected.length}, got ${arr.length}`
-        : arr.length < expected.length
-          ? `Too few elements, expected ${expected.length}, got ${arr.length}`
-          : null,
-    )
-    .pipe(
-      tuple(
-        // @ts-expect-error deliberate
-        ...expected.map((v) => makeExpecter(v)),
-      ),
-    );
-}
-
-function makeDateExpecter(expected: Date): Expecter {
-  return date.reject((d) =>
-    d.getTime() === expected.getTime()
-      ? null
-      : `Must be new Date('${expected.toISOString()}')`,
-  );
-}
-
-function makeObjectExpecter(expected: Record<PropertyKey, unknown>): Expecter {
-  return pojo.pipe(exact(mapValues(expected, makeExpecter)));
-}
-
-function makeRegexExpecter(expectedValue: RegExp): Expecter {
-  return regex(expectedValue, `Must match ${expectedValue.toString()}`);
-}
-
-const nanExpecter: Expecter = unknown.refine(
-  (x) => Number.isNaN(x as number),
-  "Must be NaN",
-);
-
-function makeExpecter(expectedValue: unknown): Expecter {
-  if (isDecoder(expectedValue)) return expectedValue;
-
-  // NaN is handled specially
-  if (Number.isNaN(expectedValue as number)) {
-    return nanExpecter;
-  }
-
-  // Arrays are handled specially
-  if (Array.isArray(expectedValue)) {
-    return makeArrayExpecter(expectedValue);
-  }
-
-  // RegExps
-  if (expectedValue instanceof RegExp) {
-    return makeRegexExpecter(expectedValue);
-  }
-
-  // Dates
-  if (expectedValue instanceof Date) {
-    return makeDateExpecter(expectedValue);
-  }
-
-  // Plain old JavaScript objects
-  if (isPojo(expectedValue)) {
-    return makeObjectExpecter(expectedValue);
-  }
-
-  // Everything else is treated as a literal value
-  return literally(expectedValue);
-}
 
 /**
  * Asserts that two values are deeply equal.
@@ -108,7 +22,7 @@ export function assertEq(
 }
 
 function assertEq_sync(actual: unknown, expected: unknown): void {
-  const result = makeExpecter(expected).decode(actual);
+  const result = makeExpector(expected).decode(actual);
   if (result.ok) return;
   fail(formatInline(result.error), assertEq);
 }
@@ -117,7 +31,7 @@ async function assertEq_async(
   actual$: PromiseLike<unknown>,
   expected: unknown,
 ): Promise<void> {
-  const result = makeExpecter(expected).decode(await actual$);
+  const result = makeExpector(expected).decode(await actual$);
   if (result.ok) return;
   fail(formatInline(result.error), assertEq);
 }
@@ -153,16 +67,4 @@ export async function assertSame_async(
   const result = literally(expected).decode(await actual$);
   if (result.ok) return;
   fail(formatInline(result.error), assertSame);
-}
-
-/**
- * Wrapper to treat the passed in schema or regex as a literal value.
- */
-export function literally(expected: unknown): Expecter {
-  // NaN is a weird one, because Object.is(NaN, NaN) is true
-  if (Number.isNaN(expected)) return nanExpecter;
-
-  // @ts-expect-error Normally constant() is used only with scalar values
-  // In this case, however, it's fine to just use it for any literal value
-  return constant(expected);
 }
---
+++ src/expectors.ts
@@ -0,0 +1,80 @@
+import type { Decoder } from "decoders";
+import {
+  constant,
+  date,
+  exact,
+  isDecoder,
+  poja,
+  pojo,
+  regex,
+  tuple,
+  unknown,
+} from "decoders";
+
+import { isPojo } from "./decoders";
+import { mapValues } from "./utils";
+
+export type Expector = Decoder<unknown>;
+
+const nanExpector: Expector = unknown.refine(
+  (x) => Number.isNaN(x as number),
+  "Must be NaN",
+);
+
+/**
+ * Wrapper to treat the passed in schema or regex as a literal value.
+ */
+export function literally(expected: unknown): Expector {
+  // NaN is a weird one, because Object.is(NaN, NaN) is true
+  if (Number.isNaN(expected)) return nanExpector;
+
+  // @ts-expect-error Normally constant() is used only with scalar values
+  // In this case, however, it's fine to just use it for any literal value
+  return constant(expected);
+}
+
+function makeArrayExpector(expected: unknown[]): Expector {
+  return poja
+    .reject((arr) =>
+      arr.length > expected.length
+        ? `Too many elements, expected ${expected.length}, got ${arr.length}`
+        : arr.length < expected.length
+          ? `Too few elements, expected ${expected.length}, got ${arr.length}`
+          : null,
+    )
+    .pipe(
+      tuple(
+        // @ts-expect-error deliberate
+        ...expected.map((v) => makeExpector(v)),
+      ),
+    );
+}
+
+function makeDateExpector(expected: Date): Expector {
+  return date.reject((d) =>
+    d.getTime() === expected.getTime()
+      ? null
+      : `Must be new Date('${expected.toISOString()}')`,
+  );
+}
+
+function makeObjectExpector(expected: Record<PropertyKey, unknown>): Expector {
+  return pojo.pipe(exact(mapValues(expected, makeExpector)));
+}
+
+function makeRegexExpector(expectedValue: RegExp): Expector {
+  return regex(expectedValue, `Must match ${expectedValue.toString()}`);
+}
+
+export function makeExpector(expectedValue: unknown): Expector {
+  // If it's already a decoder, nothing to build here
+  if (isDecoder(expectedValue)) return expectedValue;
+
+  if (Array.isArray(expectedValue)) return makeArrayExpector(expectedValue);
+  if (isPojo(expectedValue)) return makeObjectExpector(expectedValue);
+  if (expectedValue instanceof RegExp) return makeRegexExpector(expectedValue);
+  if (expectedValue instanceof Date) return makeDateExpector(expectedValue);
+
+  // Everything else is treated as a literal value
+  return literally(expectedValue);
+}
