01fcae0c93b6badcd90894618cf3c991185b6ae0
Vincent Driessen <me@nvie.com>
2025-09-24 15:04:14 +0200

    Only display console logs for failed tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    silent: "passed-only",
     coverage: {
       provider: "istanbul",
       reporter: ["text", "html"],
